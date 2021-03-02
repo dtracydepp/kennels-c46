29acc734da984b86a3a0ecbf40bdbb3bdb2d154a
dtracydepp <tracydepp82@gmail.com>
2021-03-02 11:45:53 -0600

    created customer provider

---
+++ src/components/customers/CustomerProvider.js
@@ -0,0 +1,24 @@
+import React, {useState, createContext} from "react"
+
+export const CustomerContext = createContext()
+
+export const CustomerProvider = (props) => {
+    const [customers, setCustomers] = useState([])
+
+    const getCustomers = () => {
+        return fetch("http://localhost:8088/customers")
+        .then(res => res.json())
+        .then(setCustomers)
+    }
+
+
+return (
+    <CustomerContext.Provider value={{
+        customers,getCustomers
+    }}>
+
+        {props.children}
+    </CustomerContext.Provider>
+)
+
+}
