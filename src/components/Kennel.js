2c16bfe2506186351611b775ecfda9d3f42c51bd
dtracydepp <tracydepp82@gmail.com>
2021-03-05 09:46:42 -0600

    enabled login and registration forms

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -1,11 +1,33 @@
-import React from "react"
-import { NavBar } from "./nav/NavBar"
-import { ApplicationViews } from "./ApplicationViews"
-import "./Kennel.css"
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import { ApplicationViews } from "./ApplicationViews";
+import { NavBar } from "./nav/NavBar.js";
+import { Login } from "../components/auth/Login.js";
+import { Register } from "../components/auth/Register.js";
+import "./Kennel.css";
 
 export const Kennel = () => (
-    <>
-        <NavBar />
-        <ApplicationViews />
-    </>
-)
+  <>
+    <Route
+      render={() => {
+        if (localStorage.getItem("kennel_customer")) {
+          return (
+            <>
+              <NavBar />
+              <ApplicationViews />
+            </>
+          );
+        } else {
+          return <Redirect to="/login" />;
+        }
+      }}
+    />
+
+    <Route path="/login">
+      <Login />
+    </Route>
+    <Route path="/register">
+      <Register />
+    </Route>
+  </>
+);
